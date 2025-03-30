f542b3a54dcf2708a438ea65a68d13b68302462f
Jalen Greene <terrell.greene95@gmail.com>
2025-03-30 13:00:45 -0500

    assets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,24 @@
-// import { getPayload } from "payload";
-// import config from "@/payload/payload.config";
+import { getPayload } from "payload";
+import config from "@/payload/payload.config";
 import styles from "./Header.module.scss";
-// import { Media } from "@/payload/payload-types";
-// import Image from "next/image";
-// import Link from "next/link";
+import { Media } from "@/payload/payload-types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Header() {
-  // const payload = await getPayload({ config });
+  const payload = await getPayload({ config });
 
-  // const { logo } = await payload.findGlobal({
-  //   slug: "header",
-  // });
+  const { logo } = await payload.findGlobal({
+    slug: "header",
+  });
 
-  // const { url, alt, width, height } = logo as Media;
+  const { url, alt, width, height } = logo as Media;
 
   return (
     <header className={styles.header}>
-      {/* <Link href="/">
+      <Link href="/">
         <Image src={url!} alt={alt} width={200} height={24} />
-      </Link> */}
+      </Link>
     </header>
   );
 }
